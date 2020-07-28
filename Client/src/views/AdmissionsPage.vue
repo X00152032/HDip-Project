5e35171e0b7ec6004246685e6ef6a83016f109db
Rob <X00152032@mytudublin.ie>
2020-07-28 12:33:45 +0100

    Added Pages/links (again)

---
+++ Client/src/views/AdmissionsPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="admissions">
+    <h1>Admissions</h1> <hr>
+    <h3> IMPORTANT INFORMATION FOR PARENTS/GUARDIANS </h3><br>
+
+<p>
+<br>
+Due to the commencement of the Education (Admission to Schools) Act 2018, some changes are currently underway in relation to application procedures in Irish schools, for the years from 2021/22 onwards.
+
+The attached form is an ‘Expression of Interest’ form rather than an ‘Application’ form.
+
+In September of the year prior to a student’s entry into the school (i.e. September of sixth class), we will write to those parents from whom we have received an Expression of Interest form, and we will invite them to make an application to the school on or after October 1st of that year, the earliest date that the Act allows applications to be accepted.  
+
+When applications have closed, places will then be allocated in accordance with the selection criteria laid down in the school’s Admissions’ Policy.  Parents/Guardians will be notified of whether or not their daughter has a place in the school within three weeks of the closing date for applications.
+
+<br><br>
+Please note that the Act does not allow for the formulation of a waiting list in advance of the date of application each year, and so applicants will only be ranked in accordance with the selection criteria after the closing date for applications has passed.  No places can be offered before this time. 
+
+Limited exemptions to the above are in place  for students for the school year 2021/22 to whom places have already been offered, and applications from 2022/23 until the 2025/26 school year, where parents had already applied before 1st February 2020, and where the student in question had been placed on a waiting list before this date.  
+
+A new Admissions’ Policy is being drafted under the terms of the Act and will be in place by September 2020.  The selection criteria for the allocation of places in the school will be set down in the policy.
+
+For further clarification in relation to admission to the school, please contact the Principal at principal@straphaelas.ie 
+</p>
+  </div>
+</template>
