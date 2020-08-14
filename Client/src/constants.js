8c32b96b5a75304600d14888c009fd4b5b7c7a05
Rob <X00152032@mytudublin.ie>
2020-08-14 18:16:31 +0100

    Home and subject page selecting
    Home and subject page selecting from same table but using boolean flag to decide where they go

--- Client/src/constants.js
+++ Client/src/constants.js
@@ -1,5 +1,5 @@
 const serverDetails = {
-    //url: `http://localhost:3000/`,
+   // url: `http://localhost:3000/`,
     url: `https://x00152032-server.azurewebsites.net:443/`,
     params: {
         origin: "*"
